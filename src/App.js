9e39cf35bf8fb8609ab23b98693f3b1964c28688
Nikolay <schumackov.nikolai2016@yandex.ru>
2022-08-18 23:27:28 +1000

    semicircle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MyApp from "./Clock";
+import Semicircle from "./Semicircle";
 
 const App = ()=>{
   return(
     <>
     <MyApp/>
+    <Semicircle/>
     </>
   )
 }
---
+++ src/Semicircle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Semicircle = ()=>{
+    return(
+        <>
+        <div className="sem"></div>
+        </>
+    )
+}
+export default Semicircle
